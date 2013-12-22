2081652d72a6f895d43106b4ad24e412ab21d32e
Lauricio Su <me@lauricio.com>
2013-12-23 01:00:36 +0200

    added helpers

--- client/menu.js
+++ client/menu.js
@@ -4,8 +4,27 @@ var snapper;
     'click .js-openLeft' : function () {
       // console.log(snapper)
       snapper.open('left')
+  },
+  'focus input' :function () {
+      console.log("fucus")
+      $( "#body" ).addClass( "fixfixed" );
+  },
+    'blur input' :function (e) {
+      console.log("blur")
+      e.stopImmediatePropagation();
+      $( "#body" ).removeClass( "fixfixed" );
+      $(window).scrollTop(0);
+  },
+    'click #openModal-1, tap #openModal-1' :function () {
+      $( "#modal-1" ).toggleClass( "md-show" );
+  },
+    'click .md-close' :function () {
+      $( "#modal-1" ).removeClass( "md-show" );
   }
   });
+
+
+
 
 Template.hello.created = function () {
   Meteor.defer(function () {
@@ -13,13 +32,13 @@ Template.hello.created = function () {
      snapper = new Snap({
         element: document.getElementById('content')           
     });
-         window.scroller = new AppScroll({
-         toolbar: document.getElementById('toolbar'),
-         scroller: document.getElementById('content')
-     });
-        window.scroller2 = new AppScroll({
-        toolbar: document.getElementById('toolbar'),
-        scroller: document.getElementById('draw-left')
-    });
-    window.scroller.on();
+    //      window.scroller = new AppScroll({
+    //      toolbar: document.getElementById('toolbar'),
+    //      scroller: document.getElementById('content')
+    //  });
+    //     window.scroller2 = new AppScroll({
+    //     toolbar: document.getElementById('toolbar'),
+    //     scroller: document.getElementById('draw-left')
+    // });
+    // window.scroller.on();
   })}
